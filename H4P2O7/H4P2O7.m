4e057e432b3aac7a2a9c1ade3a9922b956ea7137
IchinoseHimeki <darwinlee1998@gmail.com>
2018-10-18 23:58:05 +0800

    Regenerated the png and Fix the Matlab code

--- H4P2O7/H4P2O7.m
+++ H4P2O7/H4P2O7.m
@@ -1,6 +1,6 @@
 syms pKa1 pKa2 pKa3 pKa4 pH pKsp %%Define symbools which could probably be used.
 pH=0:0.1:14; %%set pH from 0 to 14.
-pKa1=0.7;pKa2=2.19;pKa3=6.8;pKa4=9.59;pKsp=5;
+pKa1=0.70;pKa2=2.19;pKa3=6.80;pKa4=9.59;pKsp=5.00;
 unlog=inline('(10.^(-x))','x'); %#ok<DINLN> Do the Unlog operation.
 sub1=(unlog(pH).^4+unlog(pKa1).*(unlog(pH)).^3+unlog(pKa1).*unlog(pKa2).*(unlog(pH)).^2+unlog(pKa1)...
     .*unlog(pKa2).*unlog(pKa3).*unlog(pH)+unlog(pKa1).*unlog(pKa2).*unlog(pKa3).*unlog(pKa4));
@@ -21,7 +21,7 @@ plot(pH,pH1)
 hold on
 plot(pH,pH0)
 %%
-title('ç„¦ç£·é…¸pCä¸ŽpHä¹‹é—´çš„å…³ç³»','FontSize',15,'FontName','Microsoft YaHei UI');
+title('½¹Á×ËápCÓëpHÖ®¼äµÄ¹ØÏµ','FontSize',15,'FontName','Microsoft YaHei UI');
 grid on
 xlabel('pH','FontName','Microsoft YaHei UI');
 ylabel('pC','FontName','Microsoft YaHei UI');
@@ -51,13 +51,13 @@ plot(pH,pa5)
 %Drawing for Figure2
 set(gca,'XAxisLocation','top'); %%Set x's axis to top
 set(gca,'YDir','reverse'); %% Reverse Y
-title({'ç„¦ç£·é…¸p\alphaä¸ŽpHä¹‹é—´çš„å…³ç³»'},'FontSize',15,'FontName','Microsoft YaHei UI');
+title({'½¹Á×Ëáp\alphaÓëpHÖ®¼äµÄ¹ØÏµ'},'FontSize',15,'FontName','Microsoft YaHei UI');
 grid on
 xlabel('pH','FontName','Microsoft YaHei UI');
 ylabel('p\alpha','FontName','Microsoft YaHei UI');
-legend("H_4P_2O_7","H_3P_2O_7^-","H_2P_2O_7^2-","HP_2O_7^3-","P_2O_7^4-"','Location','southwest');
+legend("H_4P_2O_7","H_3P_2O_7^-","H_2P_2O_7^2-","HP_2O_7^3-","P_2O_7^4-",'Location','southwest');
 annotation('textbox',[0.15 0 0.75 0.1],'VerticalAlignment','middle',...
-    'String',{'pK_a_1=0.7 pK_a_2=2.19 pK_a_3=6.8 pK_a_4=9.59'},...
+    'String',{'pK_a_1=0.70 pK_a_2=2.19 pK_a_3=6.80 pK_a_4=9.59'},...
     'HorizontalAlignment','center',...
     'FontName','Microsoft YaHei UI Light',...
     'FitBoxToText','off',...
